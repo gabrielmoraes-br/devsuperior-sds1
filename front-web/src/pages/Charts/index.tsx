5464bb381da4055f9ae6eb5258745051dd239086
Gabriel Moraes <gabrielmoraes.dev@gmail.com>
2020-09-19 17:43:32 -0300

    Setup front-web for production

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,7 @@ series: []
 }
 
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-gabrielrmoraes.herokuapp.com';
 
 const Charts = () => {
     const [BarChartData, setBarChartData] = useState<BarChartData[]>([]);
